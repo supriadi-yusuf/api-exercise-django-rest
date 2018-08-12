2d6f0965e9b59a1a8cf9dafb94560af8fc364700
supriadi-yusuf <supriadi96@gmail.com>
2018-08-12 18:22:38 +0700

    add viewsets as view

--- languages/views.py
+++ languages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+# spd : import moduls, class that we need
+from rest_framework import viewsets
+from .models import Language
+from .serializers import LanguageSerializer
+
 # Create your views here.
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
